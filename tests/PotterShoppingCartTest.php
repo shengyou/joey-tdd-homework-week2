2414baabc232745582d066cbbc0e526896be4456
Shengyou Fan <shengyoufan@gmail.com>
2015-10-26 20:07:35 +0800

    test2: add scenario 2 and test failed

--- tests/PotterShoppingCartTest.php
+++ tests/PotterShoppingCartTest.php
@@ -21,4 +21,18 @@ class PotterShoppingCartTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($expected, $actual);
     }
 
+    public function testBuy1EpisodeOneAnd1EpisodeTwoThenShouldPay190()
+    {
+        // Arrange
+        $target = new PotterShoppingCart();
+
+        // Act
+        $expected = 190;
+        $actual = $target->add(new Book('哈利波特第一集', 100))
+                         ->add(new Book('哈利波特第二集', 100))
+                         ->checkout();
+
+        // Assert
+        $this->assertEquals($expected, $actual);
+    }
 }
