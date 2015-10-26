e02efa6a2cabebeee814c404305914f7bcd47764
Shengyou Fan <shengyoufan@gmail.com>
2015-10-26 21:10:11 +0800

    test6: add scenario 6 and test failed

--- tests/PotterShoppingCartTest.php
+++ tests/PotterShoppingCartTest.php
@@ -86,4 +86,21 @@ class PotterShoppingCartTest extends \PHPUnit_Framework_TestCase
         // Assert
         $this->assertEquals($expected, $actual);
     }
+    
+    public function testBuy1EpisodeOneAnd1EpisodeTwoAnd2EpisodeThreeThenShouldPay370()
+    {
+        // Arrange
+        $target = new PotterShoppingCart();
+
+        // Act
+        $expected = 370;
+        $actual = $target->add(new Book('哈利波特第一集', 100))
+                         ->add(new Book('哈利波特第二集', 100))
+                         ->add(new Book('哈利波特第三集', 100))
+                         ->add(new Book('哈利波特第三集', 100))
+                         ->checkout();
+
+        // Assert
+        $this->assertEquals($expected, $actual);
+    }
 }
